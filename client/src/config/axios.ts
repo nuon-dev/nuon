f53e8b67c2f05eaccb9453f294cf045a674b21a9
iubns <iubns@naver.com>
2025-12-28 06:43:55 +0900

    fix: 무한 루프 오류 수정

--- client/src/config/axios.ts
+++ client/src/config/axios.ts
@@ -9,11 +9,20 @@ export const SERVER_FULL_PATH = `${SERVER_URL}:${PORT}`
 const isBrowser = typeof window !== "undefined"
 
 axios.defaults.baseURL = SERVER_FULL_PATH
-if (isBrowser) {
-  axios.defaults.headers.common["token"] = localStorage.getItem("token") || ""
-}
-
 axios.defaults.withCredentials = true
+
+axios.interceptors.request.use(
+  (config) => {
+    if (isBrowser) {
+      const token = localStorage.getItem("token") || ""
+      config.headers["token"] = token
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 
 axios.interceptors.response.use(
   (response) => {
